530cb9ce6d0741ffcf633f8a81d9d6c847b6b0ca
Alex <adiospallicer@cifpfbmoll.eu>
2026-03-13 20:12:22 +0100

    Declaración de la clase Profesor añadiendo sus atributos y constructor

--- src/Profesor.java
+++ src/Profesor.java
@@ -1,2 +1,11 @@
 public class Profesor {
+    private String nom;
+    private String dni;
+    private String especialitat;
+
+    public Profesor(String nom, String dni, String especialitat) {
+        this.nom = nom;
+        this.dni = dni;
+        this.especialitat = especialitat;
+    }
 }
