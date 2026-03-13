19353c7c55f5455ec98e3674506ab2bb90f1b893
Alex <adiospallicer@cifpfbmoll.eu>
2026-03-13 20:07:00 +0100

    Declaración de la clase Alumno añadiendo sus atributos y constructor

--- src/Alumne.java
+++ src/Alumne.java
@@ -1,2 +1,11 @@
 public class Alumne {
+    private String nom;
+    private String dni;
+    private int edat;
+
+    public Alumne(String nom, String dni, int edat) {
+        this.nom = nom;
+        this.dni = dni;
+        this.edat = edat;
+    }
 }
