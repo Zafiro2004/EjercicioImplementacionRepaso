d8afb29f742c1f5852ddad64c8de7a3e3c9be0b5
Alex <adiospallicer@cifpfbmoll.eu>
2026-03-13 20:50:10 +0100

    Declaración de la clase Guitarra añadiendo los atributos, constructor y método

--- src/Guitarra.java
+++ src/Guitarra.java
@@ -1,3 +1,14 @@
 public class Guitarra extends Instrument{
+    private String tipusCordes;
+    private boolean teAmplificador;
 
+    public Guitarra(String codi, String nom, double preuLloguer, String tipusCordes) {
+        super(codi, nom, preuLloguer);
+        this.tipusCordes = tipusCordes;
+        this.teAmplificador = false;
+    }
+    @Override
+    public void descriure() {
+        System.out.println("Guitarra: " + this.nom + " " + this.tipusCordes);
+    }
 }
