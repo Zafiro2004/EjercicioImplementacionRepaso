788363cdfa0aef4bd0aec54a4af5b7512dd186c5
Alex <adiospallicer@cifpfbmoll.eu>
2026-03-13 20:46:59 +0100

    Declaración de la clase Instrument añadiendo los atributos, constructor y método

--- src/Instrument.java
+++ src/Instrument.java
@@ -1,2 +1,13 @@
 public abstract class Instrument {
+    protected final String codi;
+    protected String nom;
+    protected double preuLloguer;
+
+    public Instrument(String codi, String nom, double preuLloguer) {
+        this.codi = codi;
+        this.nom = nom;
+        this.preuLloguer = preuLloguer;
+    }
+
+    public abstract void descriure();
 }
