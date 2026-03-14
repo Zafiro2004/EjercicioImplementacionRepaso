01572bbdee8f1c7bf2ffb36c0f91651deaf4cf95
Zafiro2004 <alexdediospallicer@gmail.com>
2026-03-14 12:51:31 +0100

    Implementación del constructor de la clase Curs

--- src/Curs.java
+++ src/Curs.java
@@ -1,3 +1,4 @@
+import java.util.HashMap;
 import java.util.Map;
 
 public class Curs {
@@ -8,4 +9,12 @@ public class Curs {
     private final Map<String,Alumne> alumnes;
     private ServeiNotificacions serveiNotificacions; //Se podría implementar directamente en el método pero bueno
     //Aquí faltarían los instrumentos y el profesor si los quisieramos para algo
+
+    public Curs(String codi,String nom,Nivell nivell){
+        this.codi=codi;
+        this.nom=nom;
+        this.alumnes= new HashMap<>();
+
+    }
+
 }
