e6b7588b562b86c635d18e3690cb2fddd700dfc1
Zafiro2004 <alexdediospallicer@gmail.com>
2026-03-14 10:55:59 +0100

    Añadido el método enviarConfirmacio en la clase ServeiNotificacions

--- src/ServeiNotificacions.java
+++ src/ServeiNotificacions.java
@@ -1,2 +1,5 @@
 public class ServeiNotificacions {
+    public void enviarConfirmacio(Alumne a,String nomCurs){
+        System.out.println("Nom de l'alumne: " + a.getNom()+ " Curs: " + nomCurs);
+    }
 }
