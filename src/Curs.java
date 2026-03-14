4c32585f86c5761862c01381f72438ea14528131
Zafiro2004 <alexdediospallicer@gmail.com>
2026-03-14 12:48:03 +0100

    Implementación de los atributos que salen de las relaciones

--- src/Curs.java
+++ src/Curs.java
@@ -1,6 +1,11 @@
+import java.util.Map;
+
 public class Curs {
     private final String codi;
     private String nom;
     private Nivell nivell;
 
+    private final Map<String,Alumne> alumnes;
+    private ServeiNotificacions serveiNotificacions; //Se podría implementar directamente en el método pero bueno
+    //Aquí faltarían los instrumentos y el profesor si los quisieramos para algo
 }
