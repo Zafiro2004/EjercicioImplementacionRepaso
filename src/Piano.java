70f728829ee7334b274b7390160e457281c805d0
Zafiro2004 <alexdediospallicer@gmail.com>
2026-03-14 10:38:29 +0100

    Declaración de la clase Piano añadiendo los atributos constructor y método

--- src/Piano.java
+++ src/Piano.java
@@ -1,2 +1,14 @@
 public class Piano extends Instrument{
+    private int numTecles;
+    private boolean esElectronic;
+
+    public Piano(String codi, String nom, double preuLloguer, int numTecles, boolean esElectronic) {
+        super(codi, nom, preuLloguer);
+        this.numTecles = numTecles;
+        this.esElectronic = esElectronic;
+    }
+    @Override
+    public void descriure() {
+        System.out.println("Piano: " + this.nom + " " + this.numTecles + " " + this.esElectronic);
+    }
 }
