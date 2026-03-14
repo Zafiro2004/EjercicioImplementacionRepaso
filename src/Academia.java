1e83f2a3e1d5feb21ec80f133a701136f695fe17
Zafiro2004 <alexdediospallicer@gmail.com>
2026-03-14 11:54:31 +0100

    Atributos que salen mediante las relaciones

--- src/Academia.java
+++ src/Academia.java
@@ -1,6 +1,13 @@
+import java.util.List;
+import java.util.Map;
+
 public class Academia {
     private final String codi;
     private  String nom;
+
+    private final List<Curs> cursos;
+    private final List<Instrument> instruments;
+    private final Map<String,Profesor> professors;
 
 
 }
